a7d18845930695812a6f336f3ce4c98945b9e62c
psmacabulos <patrick.macabulos@gmail.com>
2026-01-02 13:01:48 +0800

    Create a program to delete binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,13 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - Delete an entire binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: Nothing
- */
+* binary_tree_delete - Delete an entire binary tree
+* @tree: Pointer to the root node of the tree
+*
+* Return: Nothing
+*/
 void binary_tree_delete(binary_tree_t *tree)
 {
-    /* TODO: Post-order free */
-    (void)tree;
+	if (tree == NULL)
+	return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
