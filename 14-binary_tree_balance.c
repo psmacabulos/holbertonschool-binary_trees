5f742b8eefbc1ec028c873bc95e4ff09fff4c621
psmacabulos <patrick.macabulos@gmail.com>
2026-01-05 15:44:18 +0800

    Fix problem on task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,31 @@
 #include "binary_trees.h"
 
+
+/**
+* binary_tree_height - measures the height of a binary tree
+* @tree: pointer to the root node of the tree to measure
+*
+* Return: height of the tree, or 0 if tree is NULL
+*
+* Description: Height is defined as the number of edges on the longest
+*              path from the node down to a leaf.
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left >= right)
+		return (left + 1);
+	return (right + 1);
+}
+
 /**
 * binary_tree_balance - Measure the balance factor of a binary tree
 * @tree: Pointer to the root node
