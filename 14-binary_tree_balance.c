f96a2b413186377c4482e1dafc338bc9c6a9f536
psmacabulos <patrick.macabulos@gmail.com>
2026-01-05 15:58:10 +0800

    Correct wrong offset on the height function of task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
 * binary_tree_height - measures the height of a binary tree
 * @tree: pointer to the root node of the tree to measure
@@ -16,8 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
